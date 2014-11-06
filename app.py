852b7fc6238e8c469d390e856470ba4169d306e0
daniel anggrianto <daniela@aweber.com>
2014-11-06 14:16:50 -0500

    app: add getting item lists

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 import os
 
 from flask import Flask
-
+from flask.ext import restful
 
 app = Flask(__name__)
+api = restful.Api(app)
 
 
-@app.route('/')
-def view_items():
-    """show list of items"""
-    return 'item 1'
+class ItemList(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(ItemList, '/items')
 
 if __name__ == '__main__':
-    if os.environ.get('ENVIRONMENT') == 'development':
+    if os.getenv('SQ1_ENV') == 'dev':
         app.debug = True
     app.run()
