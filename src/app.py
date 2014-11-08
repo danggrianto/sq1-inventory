c38093fdf66ccd6fc69bf8ea223f5da1e226866e
daniel anggrianto <daniela@aweber.com>
2014-11-07 23:07:05 -0500

    app: add mongo

--- src/app.py
+++ src/app.py
@@ -1,15 +1,33 @@
 import os
 
+from bson.json_util import dumps
 from flask import Flask
+from flask import make_response
 from flask.ext import restful
+from flask.ext.pymongo import PyMongo
+
+MONGO_URL = os.getenv('SQ1_MONGO_URL')
+
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.getenv('SQ1_MONGO_URI')
+
+mongo = PyMongo(app)
+
+
+def output_json(obj, code, headers=None):
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+DEFAULT_REPRESENTATIONS = {'application/json': output_json}
 api = restful.Api(app)
+api.representations = DEFAULT_REPRESENTATIONS
 
 
 class ItemList(restful.Resource):
     def get(self):
-        return {'hello': 'world'}
+        return [x for x in mongo.db.inventory.find()]
 
 api.add_resource(ItemList, '/items')
 
